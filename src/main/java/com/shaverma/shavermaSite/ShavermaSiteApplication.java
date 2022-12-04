5436eecaf3f3e4a0aa4691333154192d81677660
Aleg2534 <olegkost05@gmail.com>
2022-12-04 21:47:54 +0300

    OrderPredestroyMistake

--- src/main/java/com/shaverma/shavermaSite/ShavermaSiteApplication.java
+++ src/main/java/com/shaverma/shavermaSite/ShavermaSiteApplication.java
@@ -26,21 +26,24 @@ public class ShavermaSiteApplication {
 
     public static void main(String[] args) throws IOException {
 
-        SpringApplication.run(ShavermaSiteApplication.class, args);
-        ApplicationContext context =
-                new AnnotationConfigApplicationContext("com.shaverma.shavermaSite");
-        Storage storage = context.getBean(Storage.class);
-        Map<Integer,Order> orderMap = new HashMap<>();
-        for (int i = 0; i < 10; i++) {
-            Basket basket = new Basket(new HashMap<>());
-            for (int j = 0; j < 3; j++) {
-                basket.getBasket().put(i,(2-i%2)*3);
-            }
-            storage.setOrder(new Order(i,basket,123124123.312,storage.getDelivery(i)));
-            orderMap.put(i,new Order(i,basket,123124123.312,storage.getDelivery(i)));
-            storage.setUser(new User(i,"pup"+i,"abcdf","asadwq@amklsmd",storage.getOrder(i),null, Roles.BASE_USER));
-        }
+//        SpringApplication.run(ShavermaSiteApplication.class, args);
+//        ApplicationContext context =
+//                new AnnotationConfigApplicationContext("com.shaverma.shavermaSite");
+//        Storage storage = context.getBean(Storage.class);
+//        Map<Integer,Order> orderMap = new HashMap<>();
+//        Map<Integer, User> userMap = new HashMap<>();
+//        for (int i = 0; i < 10; i++) {
+//            Basket basket = new Basket(new HashMap<>());
+//            for (int j = 0; j < 3; j++) {
+//                basket.getBasket().put(i,(2-i%2)*3);
+//            }
+//            storage.setOrder(new Order(i,basket,123124123.312,storage.getDelivery(i)));
+//            orderMap.put(i,new Order(i,basket,123124123.312,storage.getDelivery(i)));
+//            userMap.put(i,new User(i,"pup"+i,"abcdf","asadwq@amklsmd",storage.getOrder(i),null, Roles.BASE_USER));
+//            storage.setUser(new User(i,"pup"+i,"abcdf","asadwq@amklsmd",storage.getOrder(i),null, Roles.BASE_USER));
+//        }
 //        ConverterJson.writeJson(orderMap);
+//        ConverterJson.writeJson(userMap);
         int i=0;
     }
 }
---
+++ src/main/java/com/shaverma/shavermaSite/utils/storage/Storage.java
@@ -0,0 +1,86 @@
+package com.shaverma.shavermaSite.utils.storage;
+
+import com.shaverma.shavermaSite.models.delivery.Delivery;
+import com.shaverma.shavermaSite.models.order.Order;
+import com.shaverma.shavermaSite.models.product.Product;
+import com.shaverma.shavermaSite.models.user.User;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component @ComponentScan(basePackages = "java.util.HashMap")
+public class Storage {
+    private Map<Integer, Delivery> deliveryMap;
+    private Map<Integer, Order> orderMap;
+    private Map<Integer, Product> productMap;
+    private Map<Integer, User> userMap;
+
+    @Autowired
+    public Storage(Map<Integer, Delivery> deliveryMap, Map<Integer, Order> orderMap,
+                   Map<Integer, Product> productMap, Map<Integer, User> userMap) {
+        this.deliveryMap = deliveryMap;
+        this.orderMap = orderMap;
+        this.productMap = productMap;
+        this.userMap = userMap;
+    }
+
+    @PostConstruct
+    public void init() throws IOException {
+        deliveryMap = (Map<Integer, Delivery>) ConverterJson.readJson(Consts.DELIVERY_PATH);
+        orderMap = (Map<Integer, Order>) ConverterJson.readJson(Consts.ORDER_PATH);
+        productMap = (Map<Integer, Product>) ConverterJson.readJson(Consts.PRODUCT_PATH);
+        userMap= (Map<Integer, User>) ConverterJson.readJson(Consts.USER_PATH);
+    }
+
+    @PreDestroy
+    public void destroy()
+    {
+        try {
+            ConverterJson.writeJson(deliveryMap);
+            ConverterJson.writeJson(orderMap);
+            ConverterJson.writeJson(productMap);
+            ConverterJson.writeJson(userMap);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void setDelivery(Delivery newDelivery) {
+        deliveryMap.put(newDelivery.getClassId(), newDelivery);
+    }
+
+    public Delivery getDelivery(int deliveryId) {
+        return deliveryMap.get(deliveryId);
+    }
+
+    public void setOrder(Order newOrder) {
+        orderMap.put(newOrder.getClassId(), newOrder);
+    }
+
+    public Order getOrder(int orderId) {
+        return orderMap.get(orderId);
+    }
+
+    public void setProduct(Product newProduct) {
+        productMap.put(newProduct.getClassId(), newProduct);
+    }
+
+    public Product getProduct(int productId) {
+        return productMap.get(productId);
+    }
+
+    public void setUser(User newUser) {
+        userMap.put(newUser.getClassId(), newUser);
+    }
+
+    public User getUser(int userId) {
+        return userMap.get(userId);
+    }
+}
