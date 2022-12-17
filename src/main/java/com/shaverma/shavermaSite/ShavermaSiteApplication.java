94ee19a96f1ce5f5ad864ff324a43ffe72d40489
Aleg2534 <olegkost05@gmail.com>
2022-12-17 12:56:50 +0300

    servlet

--- src/main/java/com/shaverma/shavermaSite/ShavermaSiteApplication.java
+++ src/main/java/com/shaverma/shavermaSite/ShavermaSiteApplication.java
@@ -21,36 +21,19 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-
-//@SpringBootApplication
+import jakarta.servlet.ServletRequest;
+//import javax.servlet.*;
+@SpringBootApplication
 public class ShavermaSiteApplication {
 
     public static void main(String[] args) throws IOException {
 
-//        SpringApplication.run(ShavermaSiteApplication.class, args);
-        AnnotationConfigApplicationContext context =
-                new AnnotationConfigApplicationContext();
-        context.register(AnnotationConfig.class);
-        context.refresh();
-        Storage storage = context.getBean(Storage.class);
-//        Map<Integer,Order> orderMap = new HashMap<>();
-//        Map<Integer, User> userMap = new HashMap<>();
-//        for (int i = 0; i < 10; i++) {
-//            Basket basket = new Basket(new HashMap<>());
-//            for (int j = 0; j < 3; j++) {
-//                basket.getBasket().put(i,(2-i%2)*3);
-//            }
-//            storage.setOrder(new Order(i,basket,123124123.312,storage.getDelivery(i)));
-//            orderMap.put(i,new Order(i,basket,123124123.312,storage.getDelivery(i)));
-//            userMap.put(i,new User(i,"pup"+i,"abcdf","asadwq@amklsmd",storage.getOrder(i),null, Roles.BASE_USER));
-//            storage.setUser(new User(i,"pup"+i,"abcdf","asadwq@amklsmd",storage.getOrder(i),null, Roles.BASE_USER));
-//        }
-//        ConverterJson.writeJson(orderMap);
-//        ConverterJson.writeJson(userMap);
-        User user = storage.getUser(9);
-        user.setRole(Roles.ADMIN);
-        storage.setUser(user);
-        context.close();
-        int i=0;
+        SpringApplication.run(ShavermaSiteApplication.class, args);
+//        AnnotationConfigApplicationContext context =
+//                new AnnotationConfigApplicationContext();
+//        context.register(AnnotationConfig.class);
+//        context.refresh();
+//        Storage storage = context.getBean(Storage.class);
+//        context.close();
     }
 }
--- src/main/java/com/shaverma/shavermaSite/mvc/AuthorizationController.java
+++ src/main/java/com/shaverma/shavermaSite/mvc/AuthorizationController.java
@@ -1,3 +1,13 @@
 package com.shaverma.shavermaSite.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class AuthorizationController {
+    @GetMapping("/gTA")
+    public String getLogin()
+    {
+        return "Authorization";
+}
 }
