2a170a99f0ffe4e05c7439aa1c5be5d6c52cbcbb
slav <vkuznetcov48@gmail.com>
2022-12-18 17:06:36 +0400

    fix

--- src/main/java/com/shaverma/shavermaSite/ShavermaSiteApplication.java
+++ src/main/java/com/shaverma/shavermaSite/ShavermaSiteApplication.java
@@ -1,27 +1,9 @@
 package com.shaverma.shavermaSite;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.shaverma.shavermaSite.models.delivery.Delivery;
-import com.shaverma.shavermaSite.models.enums.Roles;
-import com.shaverma.shavermaSite.models.order.Basket;
-import com.shaverma.shavermaSite.models.order.Order;
-import com.shaverma.shavermaSite.models.product.Product;
-import com.shaverma.shavermaSite.models.user.User;
-import com.shaverma.shavermaSite.utils.storage.AnnotationConfig;
-import com.shaverma.shavermaSite.utils.storage.Consts;
-import com.shaverma.shavermaSite.utils.storage.ConverterJson;
-import com.shaverma.shavermaSite.utils.storage.Storage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-import jakarta.servlet.ServletRequest;
 //import javax.servlet.*;
 @SpringBootApplication
 public class ShavermaSiteApplication {
